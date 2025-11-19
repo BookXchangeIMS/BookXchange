de27b1017e780edb3feaebae74296a5759305359
Mike Gazizov <michaelgylesaces@gmail.com>
2025-11-19 23:29:15 +0000

    Add JWT authentication, token management, and config settings
    Introduced models for JWT tokens, authentication logic, and a config module for managing secrets and token settings. Updated endpoints to handle sign-up, sign-in, token refresh, and authorization workflows. Adjusted `.gitignore` to exclude sensitive config files.

---
+++ backend/auth.py
@@ -0,0 +1,24 @@
+from datetime import datetime, timedelta
+from jose import jwt, JWTError
+from fastapi import HTTPException, status
+from .config import Settings
+
+SECRET_KEY = Settings().SECRET_KEY
+ALGORITHM = Settings().ALGORITHM
+ACCESS_TOKEN_EXPIRE_MINUTES = Settings().ACCESS_TOKEN_EXPIRE_MINUTES
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.now() + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+
+def verify_token(token: str):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        return payload
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token",
+        )
--- backend/main.py
+++ backend/main.py
@@ -5,21 +5,22 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from .models import *
+from .auth import *
 app = FastAPI()
 
-@app.post("/api/sign_up")
-async def sign_up():
-    pass
+@app.post("/api/sign_up", status_code=status.HTTP_201_CREATED)
+async def sign_up(sign_up_form: SignUp):
+    return create_access_token(sign_up_form.dict())
 
 
-@app.post("/api/sign_in", response_model=Token)
+@app.post("/api/sign_in", response_model=Tokens)
 async def sign_in():
     pass
 
 
-@app.post("/api/refresh_access_token", response_model=Token)
-async def refresh_access_token():
-    pass
+@app.post("/api/refresh_access_token")
+async def refresh_access_token(token):
+    return verify_token(token)
 
 
 @app.post("/api/logout")
