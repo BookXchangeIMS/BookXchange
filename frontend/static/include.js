62a19b65863c913a102bbf991e628ab449d9ee8d
marquezinejeanrocha <20231835@novaims.unl.pt>
2025-12-29 16:22:36 +0000

    Fix header/footer fetch to use full GitHub Pages path

--- frontend/static/include.js
+++ frontend/static/include.js
@@ -16,7 +16,7 @@ function loadHTML() {
     // Load Header
     const headerPlaceholder = document.getElementById('header-placeholder');
     if (headerPlaceholder) {
-        fetch('header.html')
+        fetch('/BookXchange/frontend/templates/header.html')
             .then(response => response.text())
             .then(data => {
                 headerPlaceholder.innerHTML = data;
@@ -35,7 +35,7 @@ function loadHTML() {
     // Load Footer
     const footerPlaceholder = document.getElementById('footer-placeholder');
     if (footerPlaceholder) {
-        fetch('footer.html')
+        fetch('/BookXchange/frontend/templates/footer.html')
             .then(response => response.text())
             .then(data => {
                 footerPlaceholder.innerHTML = data;
